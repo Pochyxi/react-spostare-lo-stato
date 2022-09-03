30cf3449332a54a3e2b00992a33a5e124211c555
Pochyxi <adienerlopez@gmail.com>
2022-09-03 22:20:30 +0200

    commit in master

--- src/components/Figlio.jsx
+++ src/components/Figlio.jsx
@@ -22,7 +22,7 @@ class Figlio extends React.Component {
                     <li>La prima cosa da fare è creare una funzione nel figlio che richiama la props precedentente passata e cambia lo stato del genitore</li>
                     <p>La funzione cambiaGenitore la trovate nel Figlio riga 10</p>
                     <li>Una volta creata la funzione dobbiamo creare un constructor e "bindare" cambiaGenitore()</li>
-                    <li>Ora non ci resta altro che creare un pulsante che richiamerà la funzione ed è fatta! :)</li>
+                    <li>Ora non ci resta altro che creare un pulsante che richiamerà la funzione ed è fatta!</li>
                 </ol>
                 <button className='my-2' onClick={() => {
                     this.cambiaGenitore('Sono il FIGLIO che cambia lo stato del GENITORE!')
